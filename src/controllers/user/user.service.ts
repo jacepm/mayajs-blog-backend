044b08e12c03f7ebf3a8e4ba151e5e2b0e2e6280
Jc <jcpmandawe@gmail.com>
2020-01-17 18:31:29 +0800

    Import Models and add user model

--- src/controllers/user/user.service.ts
+++ src/controllers/user/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@mayajs/core";
+import { Models } from "@mayajs/mongo";
 
 @Injectable()
 export class UserServices {
+  @Models("user") model: any;
 
   constructor() {}
-  
+
 }
