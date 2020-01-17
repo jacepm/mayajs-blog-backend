f50ae1b787c4ee31bead3c6c05da9a193e958666
Jc <jcpmandawe@gmail.com>
2020-01-17 14:21:02 +0800

    Import UserController

--- src/app.routing.module.ts
+++ src/app.routing.module.ts
@@ -1,9 +1,13 @@
 import { BlogController } from "./controllers/blog/blog.controller";
+import { UserController } from "./controllers/user/user.controller";
 
 
 export const routes = [
   {
-    controllers: [BlogController],
+    controllers: [
+      BlogController,
+      UserController
+    ],
     middlewares: [],
     path: "",
   },
