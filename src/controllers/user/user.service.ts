85cc4cb120219e113899dd7b0413581a0309b09d
Jc <jcpmandawe@gmail.com>
2020-01-17 18:35:22 +0800

    Add async get()

--- src/controllers/user/user.service.ts
+++ src/controllers/user/user.service.ts
@@ -7,4 +7,17 @@ export class UserServices {
 
   constructor() {}
 
+  async get() {
+    try {
+      const result = await this.model.find();
+
+      if(result < 1) {
+        return { status: 400, message: "Users not found!", data: [], meta: {} }
+      }
+
+      return { status: 200, message: "Users successfully fetch.", data: result, meta: {} };
+    } catch (error) {
+      return { status: 400, message: error.errmsg ? error.errmsg : error.toString(), data: [], meta: {} };
+    }
+  }
 }
