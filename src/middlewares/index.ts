f893b0f2943da725023abe524295e2d071de3377
Jc <jcpmandawe@gmail.com>
2020-01-17 18:25:58 +0800

    Add middleware authToken

---
+++ src/middlewares/index.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { environment as env } from "../environments";
+
+export function authToken(req: Request, res: Response, next: NextFunction): void {
+
+    try {
+        const unvalidatedUrls = [
+            "/user/login"
+        ];
+
+        if (unvalidatedUrls.indexOf(req.url) > -1) {
+            return next();
+        }
+
+        const header = req.headers["authorization"];
+
+        if (typeof header !== "undefined") {
+            const bearer = header.split(" ");
+            const token = bearer[1];
+            req.body.token = token;
+            jwt.verify(req.body.token, env.AUTH_TOKEN_KEY);
+            return next();
+        } else {
+            res.sendStatus(403);
+        }
+    } catch {
+        res.status(401).send("Token is not valid!");
+    }
+}
