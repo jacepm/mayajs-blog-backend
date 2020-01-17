bb79c4792b3bc07ea9d1a0ead51d904d9e3878df
Jc <jcpmandawe@gmail.com>
2020-01-17 18:21:42 +0800

    Add HASH_ID_SALT and AUTH_TOKEN_KEY

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,4 +1,6 @@
 export const environment = {
  production: false,
- DB_CONNECT: "mongodb+srv://root:root@blog-nyni0.mongodb.net/test?retryWrites=true&w=majority"
+ DB_CONNECT: "mongodb+srv://root:root@blog-nyni0.mongodb.net/test?retryWrites=true&w=majority",
+ HASH_ID_SALT: 10,
+ AUTH_TOKEN_KEY: "sample-blog",
 };
