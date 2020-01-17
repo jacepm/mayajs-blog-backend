a7c8ec9213215b0b8b932e94a83fdf3e30484cbf
Jc <jcpmandawe@gmail.com>
2020-01-17 16:00:34 +0800

    Add async get()

--- src/controllers/blog/blog.service.ts
+++ src/controllers/blog/blog.service.ts
@@ -7,4 +7,17 @@ export class BlogServices {
 
   constructor() {}
   
+  async get() {
+    try {
+      const result = await this.db.find();
+
+      if(result < 1) {
+        return { status: 400, message: "Blogs not found!", data: [], meta: {} }
+      }
+
+      return { status: 200, message: "Blogs successfully fetch." };
+    } catch (error) {
+      return { status: 400, message: error.errmsg ? error.errmsg : error.toString(), data: [], meta: {} };
+    }
+  }
 }
