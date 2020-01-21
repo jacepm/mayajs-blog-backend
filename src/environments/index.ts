0b8ad0f1ac71139f9c97b62eccdec67bbea984b8
Jc <jcpmandawe@gmail.com>
2020-01-21 15:15:47 +0800

    Add AUTH_TOKEN_EXPIRE

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -3,4 +3,5 @@ export const environment = {
  DB_CONNECT: "mongodb+srv://root:root@blog-nyni0.mongodb.net/test?retryWrites=true&w=majority",
  HASH_ID_SALT: 10,
  AUTH_TOKEN_KEY: "sample-blog",
+ AUTH_TOKEN_EXPIRE: "60"
 };
