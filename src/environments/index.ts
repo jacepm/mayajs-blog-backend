78cf64eaa5a66a090d28a07f00836d47eaa825b5
Jc <jcpmandawe@gmail.com>
2020-01-23 00:00:49 +0800

    Change AUTH_TOKEN_EXPIRE to 900000

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -3,5 +3,5 @@ export const environment = {
  DB_CONNECT: "mongodb+srv://root:root@blog-nyni0.mongodb.net/test?retryWrites=true&w=majority",
  HASH_ID_SALT: 10,
  AUTH_TOKEN_KEY: "sample-blog",
- AUTH_TOKEN_EXPIRE: "60"
+ AUTH_TOKEN_EXPIRE: "900000"
 };
