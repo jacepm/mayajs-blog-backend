a780762fee967178447ac7e067fcc79f8ca148e4
Jc <jcpmandawe@gmail.com>
2020-01-23 20:14:12 +0800

    Change send to json

--- src/controllers/blog/blog.controller.ts
+++ src/controllers/blog/blog.controller.ts
@@ -5,50 +5,38 @@ import { BlogServices } from "./blog.service";
 
 @Controller({
   model: "./blog.model",
-  route: "/blog",
+  route: "/blog"
 })
 export class BlogController {
-
   constructor(private services: BlogServices) {}
-  
+
   @Get({ path: "/", middlewares: [] })
   async get(req: Request, res: Response, next: NextFunction): Promise<void> {
     const result = await this.services.get();
-    res.status(result.status).send(result);
+    res.status(result.status).json(result);
   }
 
   @Get({ path: "/:id", middlewares: [] })
   async getId(req: Request, res: Response, next: NextFunction): Promise<void> {
     const result = await this.services.getId(req.params.id);
-    res.status(result.status).send(result);
+    res.status(result.status).json(result);
   }
 
   @Post({
     path: "/",
-    middlewares: [
-      Check("title").isString(),
-      Check("author").isString(),
-      Check("content").isString(),
-      Check("date").isDate()
-    ]
+    middlewares: [Check("title").isString(), Check("author").isString(), Check("content").isString(), Check("date").isDate()]
   })
   async post(req: Request, res: Response, next: NextFunction): Promise<void> {
     const result = await this.services.post(req.body);
-    res.status(result.status).send(result);
+    res.status(result.status).json(result);
   }
 
   @Patch({
     path: "/:id",
-    middlewares: [
-      Check("title").isString(),
-      Check("author").isString(),
-      Check("content").isString(),
-      Check("date").isDate(),
-      Check("deleted").isBoolean()
-    ]
+    middlewares: [Check("title").isString(), Check("author").isString(), Check("content").isString(), Check("date").isDate(), Check("deleted").isBoolean()]
   })
   async patch(req: Request, res: Response, next: NextFunction): Promise<void> {
     const result = await this.services.patch(req.params.id, req.body);
-    res.status(result.status).send(result);
+    res.status(result.status).json(result);
   }
 }
