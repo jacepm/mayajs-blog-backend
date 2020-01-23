f4c9f0356efc9034e61f5efc427e4f3818d30be9
Jc <jcpmandawe@gmail.com>
2020-01-23 18:49:23 +0800

    Update authToken

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,31 +2,42 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { environment as env } from "../environments";
 
-export function authToken(req: Request, res: Response, next: NextFunction, value: string): void {
-    try {
-        const unvalidatedUrls = [
-            "/user/login",
-            "/blog",
-            "/blog/" + value
-        ];
+export const authToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const id: string = req.params.id;
+  try {
+    const unvalidatedUrls = [
+      { path: "/user/login", method: "POST" },
+      { path: "/blog", method: "GET" },
+      { path: "/blog/" + id, method: "GET" }
+    ];
 
-        if (unvalidatedUrls.indexOf(req.url) > -1) {
-            return next();
-        }
+    const unprotected = unvalidatedUrls.filter(
+      url => url.path === req.url && url.method === req.method
+    );
 
-        const header = req.headers["authorization"];
+    console.log(unprotected);
 
-        if (typeof header !== "undefined") {
-            const bearer = header.split(" ");
-            const token = bearer[1];
-            req.body.token = token;
-            jwt.verify(req.body.token, env.AUTH_TOKEN_KEY);
-            return next();
-        } else {
-            res.sendStatus(403);
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(401).send("Token is not valid!");
+    if (unprotected.length > 0) {
+      return next();
     }
-}
+
+    const header = req.headers["authorization"];
+
+    if (typeof header !== "undefined") {
+      const bearer = header.split(" ");
+      const token = bearer[1];
+      req.body.token = token;
+      jwt.verify(req.body.token, env.AUTH_TOKEN_KEY);
+      return next();
+    } else {
+      res.sendStatus(403);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).send("Token is not valid!");
+  }
+};
