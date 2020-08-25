ed960e6140356602f209b9753b8f87d947176c80
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-25 13:49:34 +0800

    FEAT: Update database to databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { App } from "@mayajs/core";
 import { Mongo } from "@mayajs/mongo";
 import { routes } from "./app.routing.module";
 import { environment as env } from "./environments";
+import MongoDb from "./databases/mongo";
 
 @App({
   cors: true,
   logs: "dev",
   port: 3333,
-  database: Mongo({
-    name: "main",
-    connectionString: env.DB_CONNECT,
-  }),
+  databases: [MongoDb],
   routes,
 })
 export class AppModule {}
