7a98ae50e5cd832baee2e66ca8b1310a3a5c932a
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-25 14:21:11 +0800

    CHORE: Update formats app.routing and index

--- src/app.routing.module.ts
+++ src/app.routing.module.ts
@@ -2,13 +2,9 @@ import { BlogController } from "./controllers/blog/blog.controller";
 import { UserController } from "./controllers/user/user.controller";
 import { authToken } from "./middlewares";
 
-
 export const routes = [
   {
-    controllers: [
-      BlogController,
-      UserController
-    ],
+    controllers: [BlogController, UserController],
     middlewares: [authToken],
     path: "/api",
   },
