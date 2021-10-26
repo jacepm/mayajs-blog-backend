77ef81b29301e529e6b7a2318400a0fb4b07a16c
Jc Mandawe <jcpmandawe@gmail.com>
2021-10-26 15:54:47 +0800

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { BlogsModule } from './blogs/blogs.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
     BlogsModule,
     UsersModule,
   ],
